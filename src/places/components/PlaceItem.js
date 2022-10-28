f1be8dc5ac2c896af4106ca4066babfecb9d2c7b
kamelbenanaya <kamel.benanaya001@gmail.com>
2022-10-28 15:50:11 +0100

    add map

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import "./PlaceItem.css";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
   const openMapHandler = () => setShowMap(true);
@@ -19,7 +20,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP </h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
